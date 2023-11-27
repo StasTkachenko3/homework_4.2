4b1537c4957a06ae90fa5628824f76b5ecd37193
Stanislaw Tkachenko <stas1696@mail.ru>
2023-11-27 22:19:08 +0400

    add commit Task_06.java

---
+++ src/Task_06.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task_06 {
+    public static void main(String[] args) {
+        System.out.println("Задание №6");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();// вводим число
+
+        for (int i = 1; i <= a; ++i) {
+
+            for (int j = 1; j < i; ++j) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= 3; ++j) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            for (int j = 1; j < i + 1; ++j) {
+                System.out.print("  ");
+            }
+            System.out.print("*");
+            System.out.println("");
+
+        }
+
+    }
+}
